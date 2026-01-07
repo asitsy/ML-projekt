5f897dec7dcce0476f33617fc823454d0f61a456
Vova <volodymyrpoleshko@gmail.com>
2026-01-07 20:39:27 +0100

    Update preprocessing pipeline

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,7 @@
 # Przygotowuje dane do uczenia maszynowego ( w sensie imputacja braków danych, normalizacja cech oraz kodowanie zmiennych)
+
+import numpy as np
+import pandas as pd
+
+from src.config import TARGET_COLUMN, NUMERIC_FEATURES, CATEGORICAL_FEATURES
+
