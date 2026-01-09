ee734afa503ea01c7e2764f8ed7187f3a1cb9f61
Vova <volodymyrpoleshko@gmail.com>
2026-01-09 13:09:40 +0100

    column merging

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,30 @@
 
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
 
 from src.config import TARGET_COLUMN, NUMERIC_FEATURES, CATEGORICAL_FEATURES
 
+def build_preprocessing_pipeline() -> ColumnTransformer:
+    numeric_pipeline = Pipeline(steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("scaler", StandardScaler())
+    ])
+    
+    categorical_pipeline = Pipeline(steps =[
+        ("imputer", SimpleImputer(strategy="most_frequent")),
+        ("encoder", OneHotEncoder(handle_unknown="ignore")),
+    ])
+    
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", numeric_pipeline, NUMERIC_FEATURES),
+            ("cat", categorical_pipeline, CATEGORICAL_FEATURES),
+        ]
+    )
+
+    return preprocessor
