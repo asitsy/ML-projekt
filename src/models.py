38d9acadcd23cf5decf0f35223d2fcb36db4fecc
Vova <volodymyrpoleshko@gmail.com>
2026-01-12 12:56:33 +0100

    First imports

--- src/models.py
+++ src/models.py
@@ -1,1 +1,19 @@
 # Definiuje i trenuje modele uczenia maszynowego, w tym model bazowy oraz alternatywne klasyfikatory
+
+
+import numpy as np 
+
+# base model
+from sklearn.linear_model import LinearRegression
+
+# tuning
+from sklearn.model_selection import GridSearchCV
+
+# models learning
+from sklearn.model_selection import train_test_split
+
+# alternatywne klasyfikatory
+from sklearn.ensemble import RandomForestClassifier
+
+# alternatywne klasyfikatory
+from sklearn.svm import SVC
