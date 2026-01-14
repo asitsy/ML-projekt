5bf10724dc96b1135e27fe7e34f63f79493185fc
Vova <volodymyrpoleshko@gmail.com>
2026-01-14 21:38:29 +0100

    Def base model

--- src/models.py
+++ src/models.py
@@ -1,19 +1,27 @@
-# Definiuje i trenuje modele uczenia maszynowego, w tym model bazowy oraz alternatywne klasyfikatory
+import numpy as np
+from typing import Dict, Any, Optional
+from dataclasses import dataclass
 
-
-import numpy as np 
-
-# base model
-from sklearn.linear_model import LinearRegression
-
-# tuning
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
 
-# models learning
-from sklearn.model_selection import train_test_split
+class ModelResult:
+    name: str
+    model: Any
+    best_params: Optional[Dict[str, Any]] = None
+    
+def train_baseline_linear(
+    X_train: np.ndarray,
+    y_train: np.ndarray,
+) -> ModelResult:
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    
+    return ModelResult(
+        name ="Baseline: LinearRegression",
+        model= model,
+    )
 
-# alternatywne klasyfikatory
-from sklearn.ensemble import RandomForestClassifier
 
-# alternatywne klasyfikatory
-from sklearn.svm import SVC
+
